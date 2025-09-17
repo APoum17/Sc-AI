030b641b85582be9b97d7db07547188c94ee9dd7
Adam Poum <adpm@Adams-MacBook-Pro.local>
2025-09-17 17:41:16 -0400

    test pyQT6

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# import tkinter as tk
-# from main_window import MainWindow
+import sys
+from PyQt6.QtWidgets import QApplication, QPushButton
 
-# if __name__ == "__main__":
-#     root = tk.Tk()
-#     app = MainWindow(root)
-#     root.mainloop()
+app = QApplication(sys.argv)
 
-from tkinter import *
-from tkinter import ttk
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-root.mainloop()
+window = QPushButton("Push Me")
+window.show()
+
+app.exec()
